f0a334e4caf93e1774c8df5424d531492db91cd1
Kareem Shehab <kareem@stahlbush.com>
2024-02-29 15:58:36 -0800

    Reverting home page styling... need to update purrcaster 3d asset

--- frontend/src/components/Home/NonUserHome.tsx
+++ frontend/src/components/Home/NonUserHome.tsx
@@ -1,4 +1,3 @@
-import PostsPreview from '@/components/PostsPreview/PostsPreview';
 import { Button } from '@/components/ui/button';
 import { Link } from 'wouter';
 import { Card } from '../ui/card';
@@ -15,8 +14,15 @@ export default function NonUserHome() {
           The weather app made by cats.
         </p>
 
-        <div className='relative flex flex-row items-center justify-center w-full !my-5 !mt-[150px] p-10 '>
-          <Card className="w-5/6 md:w-full p-6 max-w-[600px] min-w-[300px] z-10">
+        <div className='!my-5 p-5'>
+          <div className='w-full flex justify-center align-middle animate-spin p-2.5'>
+            <img src='/purrcaster-home.png' className=' top-50 w-3/4 min-w-[100px] max-w-[250px]' />
+          </div>
+
+          <Card className="w-full md:w-full p-6 max-w-[500px] min-w-[300px] mt-2.5">
+            {/* create an absolute positioned triangle div that makes it's parent div look like a speech bubble */}
+            {/* <div className='absolute -bottom-6 left-2 w-[25px] h-[25px] border-solid border-b-slate-400  border-r-slate-400 border-opacity-0 border-8'></div> */}
+
             <p className='text-lg font-semibold'>
               An old soothsayer once said
             </p>
@@ -27,9 +33,6 @@ export default function NonUserHome() {
               I took that very personally and so Purrcast was built to act as a centralized place for you to post and look at photos of peoples cats in your area to determine if it's going to rain soon or maybe just drizzle.
             </p>
           </Card>
-
-          <img src='/purrcaster-home.png' className='absolute bottom-1/4 left-1/4 sm:left-1/2 md:bottom-1/6 min-w-[300px] max-w-[300px] -z-1 transition-all' />
-          <img src='/purrcaster-home.png' className='absolute bottom-1/4 right-1/4 sm:right-1/2 md:bottom-1/6 min-w-[300px] max-w-[300px] -z-1 transition-all' />
         </div>
 
 
@@ -43,14 +46,6 @@ export default function NonUserHome() {
           </Link>
         </div>
       </section>
-
-      {/* <section id="splash-prev-posts" className="w-full">
-        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-          Posts being made by other users
-        </h2>
-        <PostsPreview />
-      </section> */}
     </>
-
   )
 }
