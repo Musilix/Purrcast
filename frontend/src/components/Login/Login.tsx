ccab07822ce4db379ca00d5a4c4561a2e4558c82
Kareem Shehab <kareem@stahlbush.com>
2024-03-11 12:53:27 -0700

    Updating styling for home page components + faq and login page

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -4,6 +4,7 @@ import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { useContext } from "react";
 import { Redirect } from "wouter";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
 export default function Login() {
   const { session } = useContext(AuthContext);
@@ -12,15 +13,26 @@ export default function Login() {
   return (
     <>
       {(session && session.user) ? <Redirect to="/" /> : (
-        <div className="sm:w-4/5 md:w-1/2 lg:w-2/5 max-w-4/5">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Login to Purrcast
-          </h1>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            In order to post, you'll have to make an account. Currently, we only support creating an account with Google.
-          </p>
-          <Auth supabaseClient={__supabase__} onlyThirdPartyProviders={true} providers={['google']} appearance={{ theme: ThemeSupa }} />
-        </div>
+        <section id="login-wrap" className="w-full sm:w-2/4 md:w-1/2 max-w-md px-10">
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                  Login to Purrcast
+                </h1>
+              </CardTitle>
+            </CardHeader>
+
+            <CardContent >
+              <CardDescription>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                  In order to post, you'll have to make an account. Currently, we only support creating an account with Google.
+                </p>
+              </CardDescription>
+              <Auth supabaseClient={__supabase__} onlyThirdPartyProviders={true} providers={['google']} appearance={{ theme: ThemeSupa }} />
+            </CardContent>
+          </Card>
+        </section>
       )}
     </>
   )
