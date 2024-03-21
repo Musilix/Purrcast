6bc48fe8eaf51e9076074c12be717c7f0e21b659
Kareem Shehab <kshehab1@outlook.com>
2024-03-20 20:42:13 -0700

    Updated postCreateDTO, but wont actually be using it I think...

--- lib/schemas/PostSchema.ts
+++ lib/schemas/PostSchema.ts
@@ -1,12 +1,39 @@
 import { z } from "zod";
 
-export const createPostSchema = z.object({
-  contentId: z.string(),
-  authorId: z.string(),
-  published: z.boolean(),
-});
+// TODO - currently since nestjs provides a means to validate files sent in a form data object from the client,
+// it kinda defeats the purpose of using zod to validate the file. However, if we wanted to validate the content on the client side instead
+// before it's sent out, we could try to utilize this.
 
-export type CreatePostDto = z.infer<typeof createPostSchema>;
+// NOTE - if we'd like, we can validate size of the file buffer sent from the user on our backend
+const MAX_FILE_SIZE = 30000000;
+const checkFileType = (file: File) => {
+  if (
+    file.name.endsWith(".png") ||
+    file.name.endsWith(".jpg") ||
+    file.name.endsWith(".jpeg") ||
+    file.name.endsWith(".webp")
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+const FileType = z
+  .any()
+  .refine((file: File) => file, "You must upload a file.")
+  .refine((file: File) => {
+    file.size < MAX_FILE_SIZE;
+  }, "The file you uploaded is too large. Please upload a file that is less than 30MB.")
+  .refine((file: File) => {
+    checkFileType(file);
+  }, "The file you uploaded is not a valid image. It must be a .png, .jpg, .jpeg, or .webp file.");
+
+export const createPostSchema = z.object({
+  userUploadedFile: FileType,
+  authorId: z.string(),
+  published: z.coerce.boolean(),
+});
 
 export const TestDataSchema = z.object({
   name: z.string().max(10).min(2),
@@ -15,4 +42,5 @@ export const TestDataSchema = z.object({
   date: z.date().optional(),
 });
 
+export type CreatePostDto = z.infer<typeof createPostSchema>;
 export type TestDataDto = z.infer<typeof TestDataSchema>;
