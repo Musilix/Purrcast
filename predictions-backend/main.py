b76d8b22cef80233087c99e448808927a68855e9
Kareem Shehab <kareem@stahlbush.com>
2024-04-04 16:17:26 -0700

    Setting up reqs and endpoint for predicting cat imgs

--- predictions-backend/main.py
+++ predictions-backend/main.py
@@ -2,11 +2,22 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from transformers import pipeline
 from typing import List
+import torch 
+from PIL import ImageTk, Image
+from pydantic import BaseModel
 
-sentiment = pipeline("sentiment-analysis")
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-tagger = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+# sentiment = pipeline("sentiment-analysis")
+# summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+# tagger = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+# image_clasifier = pipeline(model="microsoft/beit-base-patch16-224-pt22k-ft22k")
+image_clasifier = pipeline(model="google/vit-base-patch16-224")
 
+class TextItem(BaseModel):
+    text: str
+
+class PostItem(BaseModel):
+    img_url: str
+        
 app = FastAPI()
 
 # origins = ["*"]
@@ -19,18 +30,26 @@ app = FastAPI()
 # )
 
 @app.get("/")
-def read_root():
+def hello_world():
     return {"Hello": "World"}
  
-@app.get("/ai/summarize")
-def get_summary(text: str):
-    return summarizer(text)
+# @app.post("/ai/summarize")
+# def get_summary(textItem: TextItem):
+#     return summarizer(textItem.text);
 
-@app.get("/ai/sentiment")
-def get_sentiment(text: str):
-    return sentiment(text)
+# @app.post("/ai/sentiment")
+# def get_sentiment(textItem: TextItem):
+#     return sentiment(textItem.text)
 
-@app.get("/healthcheck")
-def read_root():
-    return {"status": "ok"}
+@app.post('/ai/is-on-head')
+def classify_cat_image(user_post: PostItem):
+    return image_clasifier(images=user_post.img_url)
+                     
+# @app.get("/ai/sentiment")
+# def get_sentiment(textItem: TextItem):
+#     return sentiment(textItem.text)
+
+# @app.get("/healthcheck")
+# def read_root():
+#     return {"status": "ok"}
  
