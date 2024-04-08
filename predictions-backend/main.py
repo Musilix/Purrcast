a1c3c501d064b7fddeb9cd88a81807740101d74c
Kareem Shehab <kareem@stahlbush.com>
2024-04-08 16:40:39 -0700

    Adding custom model, tweaking dependencies

--- predictions-backend/main.py
+++ predictions-backend/main.py
@@ -7,18 +7,19 @@ from PIL import ImageTk, Image
 from pydantic import BaseModel
 
 # Deps for custom model
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 import cv2
 import numpy as np
 import tensorflow as tf
 import os
+from skimage import io
 
 # Available pipelines from Hugging Face
 # sentiment = pipeline("sentiment-analysis")
 # summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 # tagger = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 # image_clasifier = pipeline(model="microsoft/beit-base-patch16-224-pt22k-ft22k")
-image_clasifier = pipeline(model="google/vit-base-patch16-224")
+# image_clasifier = pipeline(model="google/vit-base-patch16-224")
 
 # Models for possible payloads sent to endpoints
 class TextItem(BaseModel):
@@ -29,7 +30,7 @@ class PostItem(BaseModel):
 
 # Necessary config stuff - create fast api app instance + classifier model 
 app = FastAPI()
-cat_classifier_model = load_model('models/isCatOnHeadClassifier.h5')
+cat_classifier_model = tf.keras.models.load_model('isCatOnHeadClassifier.h5')
 
 # CORS stuff
 # origins = ["*"]
@@ -45,12 +46,9 @@ cat_classifier_model = load_model('models/isCatOnHeadClassifier.h5')
 def hello_world():
     return {"Hello": "World"}
 
-@app.post('/ai/is-on-head')
-def classify_cat_image(user_post: PostItem):
-    return image_clasifier(images=user_post.img_url)
- 
 def custom_cat_classifer(img_url):
-    img = cv2.imread('/content/drive/MyDrive/data/notSleepingOnHead/gunt.png')
+    img = io.imread(img_url)
+    # img = cv2.imread('/content/drive/MyDrive/data/notSleepingOnHead/gunt.png')
     resize = tf.image.resize(img, (256,256))
     pred = cat_classifier_model.predict(np.expand_dims(resize/255, 0))
 
@@ -58,6 +56,11 @@ def custom_cat_classifer(img_url):
         return 'We think the cat is on it\'s head'
     else:
         return 'We think the cat is NOT on it\'s head'
+
+# @app.post('/ai/is-on-head')
+# def classify_cat_image(user_post: PostItem):
+#     return image_clasifier(images=user_post.img_url)
+ 
 
 # @app.post("/ai/summarize")
 # def get_summary(textItem: TextItem):
