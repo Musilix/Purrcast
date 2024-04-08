78ff6e35e609e5c7dd7d9beabd2029cf125d16a8
Kareem Shehab <kareem@stahlbush.com>
2024-04-08 16:09:55 -0700

    Scaffolding new approach. Pausing on Hugging Face, trying to utilize raw custom model

--- predictions-backend/main.py
+++ predictions-backend/main.py
@@ -6,20 +6,32 @@ import torch
 from PIL import ImageTk, Image
 from pydantic import BaseModel
 
+# Deps for custom model
+from tensorflow.keras.models import load_model
+import cv2
+import numpy as np
+import tensorflow as tf
+import os
+
+# Available pipelines from Hugging Face
 # sentiment = pipeline("sentiment-analysis")
 # summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 # tagger = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 # image_clasifier = pipeline(model="microsoft/beit-base-patch16-224-pt22k-ft22k")
 image_clasifier = pipeline(model="google/vit-base-patch16-224")
 
+# Models for possible payloads sent to endpoints
 class TextItem(BaseModel):
     text: str
 
 class PostItem(BaseModel):
     img_url: str
-        
-app = FastAPI()
 
+# Necessary config stuff - create fast api app instance + classifier model 
+app = FastAPI()
+cat_classifier_model = load_model('models/isCatOnHeadClassifier.h5')
+
+# CORS stuff
 # origins = ["*"]
 # app.add_middleware(
 #     CORSMiddleware,
@@ -32,7 +44,21 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"Hello": "World"}
+
+@app.post('/ai/is-on-head')
+def classify_cat_image(user_post: PostItem):
+    return image_clasifier(images=user_post.img_url)
  
+def custom_cat_classifer(img_url):
+    img = cv2.imread('/content/drive/MyDrive/data/notSleepingOnHead/gunt.png')
+    resize = tf.image.resize(img, (256,256))
+    pred = cat_classifier_model.predict(np.expand_dims(resize/255, 0))
+
+    if pred > 0.5:
+        return 'We think the cat is on it\'s head'
+    else:
+        return 'We think the cat is NOT on it\'s head'
+
 # @app.post("/ai/summarize")
 # def get_summary(textItem: TextItem):
 #     return summarizer(textItem.text);
@@ -40,11 +66,7 @@ def hello_world():
 # @app.post("/ai/sentiment")
 # def get_sentiment(textItem: TextItem):
 #     return sentiment(textItem.text)
-
-@app.post('/ai/is-on-head')
-def classify_cat_image(user_post: PostItem):
-    return image_clasifier(images=user_post.img_url)
-                     
+               
 # @app.get("/ai/sentiment")
 # def get_sentiment(textItem: TextItem):
 #     return sentiment(textItem.text)
@@ -52,4 +74,3 @@ def classify_cat_image(user_post: PostItem):
 # @app.get("/healthcheck")
 # def read_root():
 #     return {"status": "ok"}
- 
