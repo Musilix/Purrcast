52c677b3c012316c9468697634f715e204e0b255
Kareem Shehab <kareem@stahlbush.com>
2024-06-04 15:32:16 -0700

    trying to play around with animations, idk!

---
+++ frontend/src/components/Posts/PostListFilter/PostListFilter.tsx
@@ -0,0 +1,11 @@
+// import { motion } from 'framer-motion';
+
+export default function PostListFilter() {
+  return (
+    <>
+      {/* <motion.div>
+
+            </motion.div> */}
+    </>
+  );
+}
--- frontend/src/components/Test/Test.tsx
+++ frontend/src/components/Test/Test.tsx
@@ -1,33 +1,68 @@
-// import { Button } from "@/components/ui/button";
-import { Button } from '../ui/button';
-import { useToast } from '../ui/use-toast';
+import { useState } from 'react';
+import { motion, Variants } from 'framer-motion';
 
+const itemVariants: Variants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: { type: 'spring', stiffness: 300, damping: 24 },
+  },
+  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+};
 export default function Test() {
-  const { toast } = useToast();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <Button onClick={() => toast({ description: 'Hello World!' })}>
-        Click me
-      </Button>
-      <Button
-        variant="destructive"
-        onClick={() =>
-          toast({
-            title: 'There was an issue processing your request',
-            description: 'Hello World!',
-            variant: 'destructive',
-          })
-        }
+    <motion.nav initial={false} animate={isOpen ? 'open' : 'closed'}>
+      <motion.button
+        whileTap={{ scale: 0.97 }}
+        onClick={() => setIsOpen(!isOpen)}
+        className="w-full flex flex-row justify-between bg-blue-300 p-5 border border-gray-200 rounded-t-lg text-black text-lg"
       >
-        Click me
-      </Button>
-      <Button
-        variant="outline"
-        onClick={() => toast({ description: 'Hello World!' })}
+        Menu
+        <motion.div
+          variants={{
+            open: { rotate: 180 },
+            closed: { rotate: 0 },
+          }}
+          transition={{ duration: 0.2 }}
+          style={{ originY: 0.55 }}
+        >
+          <svg width="15" height="15" viewBox="0 0 20 20" color="white">
+            <path d="M0 7 L 20 7 L 10 16" />
+          </svg>
+        </motion.div>
+      </motion.button>
+      <motion.ul
+        variants={{
+          open: {
+            clipPath: 'inset(0% 0% 0% 0% round 10px)',
+            transition: {
+              type: 'spring',
+              bounce: 0,
+              duration: 0.7,
+              delayChildren: 0.1,
+              staggerChildren: 0.2,
+            },
+          },
+          closed: {
+            clipPath: 'inset(10% 50% 90% 50% round 10px)',
+            transition: {
+              type: 'spring',
+              bounce: 0,
+              duration: 0.3,
+            },
+          },
+        }}
+        style={{ pointerEvents: isOpen ? 'auto' : 'none' }}
+        className="*:w-full *:p-5 *:border  first:bg-green-300! last:rounded-b-lg"
       >
-        Click me
-      </Button>
-    </>
+        <motion.li variants={itemVariants}>Item 1 </motion.li>
+        <motion.li variants={itemVariants}>Item 2 </motion.li>
+        <motion.li variants={itemVariants}>Item 3 </motion.li>
+        <motion.li variants={itemVariants}>Item 4 </motion.li>
+        <motion.li variants={itemVariants}>Item 5 </motion.li>
+      </motion.ul>
+    </motion.nav>
   );
 }
